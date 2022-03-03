db9279e3bb75e99ceaab4e3b05c66a574fa4e7bb
Thays <94051605+ThayRibeiro0@users.noreply.github.com>
2022-03-02 23:27:59 -0300

    Update aprendendo.js

--- aprendendo.js
+++ aprendendo.js
@@ -1,21 +1,18 @@
-const mongoose = require("mongoose")
+//Conexão banco Atlas
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://138.255.111.58:<usbW2391>@cluster0.6p9tk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 client.connect(err => {
     const collection = client.db("test").collection("devices");
     // perform actions on the collection object
     client.close();
   });
 
+//Conexão Banco Terminal
+const mongoose = require("mongoose")
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://138.255.111.58", {
-    useMongoClient: true
-}).then(() => {
-    console.log("MongoDB Conectado...")
-}).catch((err) => {
-    console.log("Houve um erro ao se conectar ao mongoDB: "+err)
+mongoose.connect("mongodb://138.255.111.58", {useMongoClient: true
+}).then(() => {console.log("MongoDB Conectado...")}).catch((err) => {console.log("Houve um erro ao se conectar ao mongoDB: "+err)
 })
 
 // Model - Usuários
@@ -37,5 +34,23 @@ const UserSchema = mongoose.Schema({
     idade: {
         type: Number,
         require: true
+    },
+    pais: {
+        type: Number,
     }
 })
+
+//Collection
+    mongoose.model('usuarios', UserSchema)
+
+    new UserSchema({
+        nome: "Victor",
+        sobrenome: "Lima",
+        email: "email@email.com",
+        idade: 19,
+        pais: "Brasil"
+    }).save().then(() => {
+        console.log("Usuário criado com sucesso!")
+    }).catch((err) => {
+        console.log("Houve um erro ao registrar o usuário: "+err)
+    }) 
